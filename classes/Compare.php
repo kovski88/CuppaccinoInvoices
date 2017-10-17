9e8c43d6344d2f4bf4963770ab6a7c6cfc0fd6b3
MSkeltonTS <mark.skelton@trueshopping.co.uk>
2017-10-17 11:05:54 +0100

    Change format, duplicate

--- classes/Compare.php
+++ classes/Compare.php
@@ -17,21 +17,11 @@ class Compare
 
                 //CSV line (sku - price)
                 $array = str_getcsv($line);
-		if($masterPound)
-		{
-			if (strpos($array[$masterPrice], "£") !== false) 
-                        { 
-				$masterAmount = trim(preg_replace('/[^0-9.]+/', '', $array[$masterPrice]));
-				if(is_numeric($masterAmount)){
-					$masterList[trim($array[$masterSku])]['price'] = $masterAmount;
-				}
-			}
-		} else {
-			$masterAmount = trim(preg_replace('/[^0-9.]+/', '', $array[$masterPrice]));
-			if(is_numeric($masterAmount)){
-	                        $masterList[trim($array[$masterSku])]['price'] = $masterAmount;
-			}
-		}
+
+                $masterAmount = trim(preg_replace('/[^0-9.]+/', '', $array[$masterPrice]));
+                if(is_numeric($masterAmount)){
+                    $masterList[trim($array[$masterSku])]['price'] = $masterAmount;
+                }
             }
             fclose($handle);
         } else {
@@ -50,21 +40,11 @@ class Compare
 
                 //CSV line (sku - qty - price)
                 $array = str_getcsv($line);
-		if($invoicePound)
-		{
-                	if (strpos($array[$invoicePrice], "£") !== false) 
-			{
-				$invoiceAmount = trim(preg_replace('/[^0-9.]+/', '', $array[$invoicePrice]));
-				if(is_numeric($invoiceAmount)){
-	                    		$invoiceList[trim($array[$invoiceSku])]['price'] = $invoiceAmount; //($array[2]/$array[1]);
-				}
-                	}
-		} else {
-			$invoiceAmount = trim(preg_replace('/[^0-9.]+/', '', $array[$invoicePrice]));
-			if(is_numeric($invoiceAmount)){
-                        	$invoiceList[trim($array[$invoiceSku])]['price'] = $invoiceAmount; //($array[2]/$array[1]);	
-			}
-		}
+
+                $invoiceAmount = trim(preg_replace('/[^0-9.]+/', '', $array[$invoicePrice]));
+                if(is_numeric($invoiceAmount)){
+                    $invoiceList[trim($array[$invoiceSku])]['price'] = $invoiceAmount; //($array[2]/$array[1]);
+                }
             }
             fclose($handle);
         } else {
@@ -81,8 +61,8 @@ class Compare
         $invoiceItemsPriceMismatch_Expensive = array();
         $invoiceItemsMatch = array();
         $invoiceItemsPriceMismatch_Unknown = array();
-	    $overChargeTotal = 0;
-	    $underChargeTotal = 0;
+        $overChargeTotal = 0;
+        $underChargeTotal = 0;
 
         foreach ($invoiceList as $sku => $invoiceItem) {
             if (!isset($masterList[$sku])) {
@@ -91,34 +71,34 @@ class Compare
                 $priceMaster = $masterList[$sku]['price'];
                 $pricePaid = $invoiceItem['price'];
 
-		if($priceDifference > 0)
-		{
-			//convert to pence
-			$priceDifferencePence = $priceDifference / 100;
-		}
-		$priceMasterHigh = $priceMaster + $priceDifferencePence;
-		$priceMasterLow = $priceMaster - $priceDifferencePence;
+                if($priceDifference > 0)
+                {
+                    //convert to pence
+                    $priceDifferencePence = $priceDifference / 100;
+                }
+                $priceMasterHigh = $priceMaster + $priceDifferencePence;
+                $priceMasterLow = $priceMaster - $priceDifferencePence;
 
-		if ($pricePaid <= $priceMasterHigh && $pricePaid >= $priceMasterLow) {
-			if($priceDifference > 0)
-                    	{
-                        	$invoiceItemsMatch[$sku] = "Price match or within threshold of {$priceDifference}p";
-                    	} else {
-                        	$invoiceItemsMatch[$sku] = "Price match";
-                	}
-		} elseif ($priceMasterHigh < $pricePaid) {
-		    //Items in Invoice are more expensive than price master + threshold:
+                if ($pricePaid <= $priceMasterHigh && $pricePaid >= $priceMasterLow) {
+                    if($priceDifference > 0)
+                    {
+                        $invoiceItemsMatch[$sku] = "Price match or within threshold of {$priceDifference}p";
+                    } else {
+                        $invoiceItemsMatch[$sku] = "Price match";
+                    }
+                } elseif ($priceMasterHigh < $pricePaid) {
+                    //Items in Invoice are more expensive than price master + threshold:
                     $invoiceItemsPriceMismatch_Expensive[$sku]['text'] = "Master price at $priceMaster but invoiced at $pricePaid";
-		    $expensiveDifference = number_format($pricePaid - $priceMaster, 2);
-		    $invoiceItemsPriceMismatch_Expensive[$sku]['diff'] = $expensiveDifference;
-		    $overChargeTotal += $expensiveDifference;
+                    $expensiveDifference = number_format($pricePaid - $priceMaster, 2);
+                    $invoiceItemsPriceMismatch_Expensive[$sku]['diff'] = $expensiveDifference;
+                    $overChargeTotal += $expensiveDifference;
 
                 } elseif ($priceMasterLow > $pricePaid) {
-		    //Item in Invoice are cheaper than price master.
+                    //Item in Invoice are cheaper than price master.
                     $invoiceItemsPriceMismatch_Cheaper[$sku]['text'] = "Master price at $priceMaster but invoiced at $pricePaid";
-		    $cheaperDifference = number_format($priceMaster - $pricePaid, 2);
-		    $invoiceItemsPriceMismatch_Cheaper[$sku]['diff'] = $cheaperDifference;
-		    $underChargeTotal += $cheaperDifference;
+                    $cheaperDifference = number_format($priceMaster - $pricePaid, 2);
+                    $invoiceItemsPriceMismatch_Cheaper[$sku]['diff'] = $cheaperDifference;
+                    $underChargeTotal += $cheaperDifference;
                 } else {
                     $invoiceItemsPriceMismatch_Unknown[$sku] = "Unknown error";
                 }
@@ -132,8 +112,8 @@ class Compare
             $invoiceItemsPriceMismatch_Expensive,
             $invoiceItemsMatch,
             $invoiceItemsPriceMismatch_Unknown,
-	        $overChargeTotal,
-	        $underChargeTotal,
+            $overChargeTotal,
+            $underChargeTotal,
         );
 
 
